4a02c7dd505a4429241329a3e6aef646804488ed
Jochem Boersma <jochem.boersma@witteveenbos.com>
2025-08-27 11:20:12 +0200

    deployment setup

---
+++ IAK_Report/__init__.py
@@ -0,0 +1,26 @@
+"""
+IAK Reporting Tool
+
+Automated reporting tools for IAK (Instandhouding Advisering Kunstwerken) project.
+Generates PI reports, attention points, and risk assessments from DISK exports.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Arcadis IAK Team"
+__email__ = "iak-team@arcadis.com"
+
+# Import main modules for easier access
+from . import utils
+from . import utilsxls
+from . import get_voortgang
+
+__all__ = [
+    "utils",
+    "utilsxls", 
+    "get_voortgang",
+    "generate_pi_rapportage",
+    "generate_aandachtspunten_beheerder",
+    "generate_hoogste_risicos",
+    "export_excel_to_pdf",
+    "ora_to_word",
+]
